5e51bdf7f8b8fed9dfbac21e37208dccbcaf0540
Abeer Zahid <azahid@ssidecisions.com>
2022-04-21 17:38:02 +0500

    Login: Update UI

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import StoreProvider from './store/StoreProvider';
 import {useSelector} from "react-redux";
 import { createTheme, ThemeProvider } from '@material-ui/core/styles';
 //import { StyledEngineProvider } from '@material-ui/material/styles';
+import variables from "./assets/scss/_variables.scss"
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/scss/styles.scss';
 
@@ -14,6 +15,12 @@ const ContentWrapper = ({children}) => {
     const theme = React.useMemo(
         () => createTheme({
                 palette: {
+                    primary: {
+                        // light: will be calculated from palette.primary.main,
+                        main: variables.primary,
+                        // dark: will be calculated from palette.primary.main,
+                        // contrastText: will be calculated to contrast with palette.primary.main
+                      },
                     type: isDarkMode ? 'dark' : 'light',
                 },
             }),
