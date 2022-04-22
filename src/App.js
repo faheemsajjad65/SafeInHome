bdb05a662729d82b82d19bcc66b6035b8949f91e
Abeer Zahid <azahid@ssidecisions.com>
2022-04-22 11:27:34 +0500

    App: Manual merge

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Settings from "./views/Settings"
 import StoreProvider from './store/StoreProvider';
 import {useSelector} from "react-redux";
 import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+//import { StyledEngineProvider } from '@material-ui/material/styles';
+import variables from "./assets/scss/_variables.scss"
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/scss/styles.scss';
 
 
 function Authenticate({children}){
@@ -19,6 +23,12 @@ const ContentWrapper = ({children}) => {
     const theme = React.useMemo(
         () => createTheme({
                 palette: {
+                    primary: {
+                        // light: will be calculated from palette.primary.main,
+                        main: variables.primary,
+                        // dark: will be calculated from palette.primary.main,
+                        // contrastText: will be calculated to contrast with palette.primary.main
+                    },
                     type: isDarkMode ? 'dark' : 'light',
                 },
             }),
@@ -43,7 +53,10 @@ function SihWizard (){
 }
 
 function App() {
-  return (
+    return (
+      // <StyledEngineProvider injectFirst>
+      //   <SihWizard/>
+      // </StyledEngineProvider>
       <StoreProvider>
         <SihWizard/>
       </StoreProvider>
