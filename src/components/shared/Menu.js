ca6c9d88a8f9a33a89242c77cff0a75e6f9b342b
Abeer Zahid <azahid@ssidecisions.com>
2022-04-26 14:25:38 +0500

    Fix className attr

--- src/components/shared/Menu.js
+++ src/components/shared/Menu.js
@@ -2,30 +2,22 @@ import React from 'react'
 
 export default function Menu() {
   return (
-    <div class="app-sidebar">
-
-      
-      <ul class="navbar-nav sidebar sidebar-dark accordion toggled" id="accordionSidebar">
-        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-            <div class="sidebar-brand-icon rotate-n-15">
-                <i class="fas fa-laugh-wink"></i>
-            </div>
-            <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
-        </a>
-        <li class="nav-item active">
-            <a class="nav-link" href="#">
-            <span class="icon icon-wizard"></span>
+    <div className="app-sidebar">
+      <ul className="navbar-nav sidebar sidebar-dark accordion toggled" id="accordionSidebar">
+        <li className="nav-item active">
+            <a className="nav-link" href="#">
+            <span className="icon icon-wizard"></span>
                 <span>Wizard</span></a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span class="icon icon-users"></span>
+        <li className="nav-item">
+            <a className="nav-link" href="#">
+              <span className="icon icon-users"></span>
               <span>Team</span>
             </a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span class="icon icon-settings"></span>
+        <li className="nav-item">
+            <a className="nav-link" href="#">
+              <span className="icon icon-settings"></span>
               <span>Settings</span>
             </a>
         </li>
--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import Avatar from '@material-ui/core/Avatar';
+import AvatarSrc from "../../assets/images/avatar-user.jpg";
 import LogoSrc from "../../assets/images/logo.svg";
 
 export default function Navbar() {
   return (
-  <div class="app-header">
-    <nav class="navbar navbar-dark bg-dark fixed-top">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
+  <div className="app-header">
+    <nav className="navbar navbar-dark bg-dark fixed-top">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
           <img alt="brand-logo" src={LogoSrc} width="30" height="24"/>
         </a>
+        <Avatar className="user-image" alt="User Image" src={AvatarSrc}/>
       </div>
     </nav>
   </div>
