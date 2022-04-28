c2dfc7bed82cd392855f645b6783836b8ccfd314
Faheem Sajjad <faheem@poocho.co>
2022-04-28 20:25:49 +0500

    implementation of modal

---
+++ src/reducers/modal.js
@@ -0,0 +1,20 @@
+import {
+    TOGGLE_MODAL
+} from "../types";
+
+const initialState = {
+    isModalOpen: false,
+}
+export default function ModalReducer (state = initialState, action) {
+    const { type, payload } = action;
+
+    switch (type) {
+        case TOGGLE_MODAL:
+            return {
+                ...state,
+                isModalOpen: payload
+            }
+        default:
+            return state;
+    }
+} 
